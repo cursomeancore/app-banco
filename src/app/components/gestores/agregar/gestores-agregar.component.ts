7ab1b9a0e63dfc43c222bc02084ac872ad8ecdaa
atalaminos <atalaminos@gmail.com>
2020-02-18 11:54:27 +0100

    12. Implementar la lógica asociada a la agregación de un gestor

--- src/app/components/gestores/agregar/gestores-agregar.component.ts
+++ src/app/components/gestores/agregar/gestores-agregar.component.ts
@@ -1,9 +1,13 @@
+import { HttpGestorService } from '../../../services/http-gestor.service';
+import { catchError } from 'rxjs/operators';
+import { AlertService } from '../../../services/alert.service';
+
 import {
   Component,
-  OnInit,
   ElementRef,
   ViewChild
 } from '@angular/core';
+import { TIPO_ALERTA } from '../../../data/alerta';
 
 
 @Component({
@@ -11,16 +15,41 @@ import {
   templateUrl: './gestores-agregar.component.html',
   styleUrls: ['./gestores-agregar.component.css']
 })
-export class GestoresAgregarComponent implements OnInit {
+export class GestoresAgregarComponent {
   @ViewChild('usuarioInput', { static: false }) nameInputRef: ElementRef;
   @ViewChild('passwordInput', { static: false }) amountInputRef: ElementRef;
 
-  constructor() { }
+  constructor(private httpGestorService: HttpGestorService, private alertService: AlertService) { }
 
-  ngOnInit() {
-  }
+  onAgregarGestor(usuarioInput: HTMLInputElement,
+                  passwordInput: HTMLInputElement,
+                  correoInput: HTMLInputElement) {
 
-  onAgregarGestor() {
+    const usuario = {
+      usuario: usuarioInput.value,
+      password: passwordInput.value,
+      correo: correoInput.value,
+    }
 
+    this.httpGestorService.agregarGestor(usuario)
+    .subscribe(() => {
+      this.alertService.enviarAlerta({
+        texto: 'Gestor agregado correctamente',
+        tipo: TIPO_ALERTA.SUCCESS,
+        tiempo: 4000
+      });
+
+      // resetear todos los campos
+      usuarioInput.value = '';
+      passwordInput.value = '';
+      correoInput.value = '';
+
+    }, (err) => {
+      this.alertService.enviarAlerta({
+        texto: err,
+        tipo: TIPO_ALERTA.DANGER,
+        tiempo: 4000
+      });
+    });
   }
 }
